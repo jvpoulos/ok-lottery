9a4d344b90f2ccb63b28d9bcd1e265147f2f3fd2
jvpoulos <jvpoulos@gmail.com>
2017-02-22 14:35:53 -0800

    finalize power analysis

--- code/ok-power.R
+++ code/ok-power.R
@@ -7,21 +7,22 @@ require(ggplot2)
 # Define simulation parameters
 
 alpha <- 0.05 # significance level 
-L <- 100 # no. iterations
+L <- 1000 # no. iterations
 r.prob <- c(0.0125,0.025,0.05,0.1) # effect size for binary response
 s.size <- c(500, 1000, 2000, 4000, 8000, 13000) # sample size
 
 lambda <- 0.001 # decay rate of exponential function
 n <- 6500 # county pop. size
 comply <- 0.9 # compliance rate
+amp <- c(0,1,3) # amplify to affect noise
 
 # Create grid for parameters
 
-grid.bin <- expand.grid("r.prob"=r.prob, "s.size"=s.size)
+grid.bin <- expand.grid("r.prob"=r.prob, "s.size"=s.size, "amp"=amp)
 
 # Define regression simulation function
 
-SimRegression <- function(r.prob,s.size,lambda,n,comply){
+SimRegression <- function(r.prob,s.size,lambda,n,comply,amp){
   # Simulate draw number, compliers, and response independently by county
   
   n <- s.size/2 # split sample size by number of counties
@@ -38,14 +39,20 @@ SimRegression <- function(r.prob,s.size,lambda,n,comply){
   # draw response from binom dis
   # treatment effect decays exponentially as a function of lambda and draw number
   # non-compliers do not have treatment effects
-  response <- c(rbinom(n, 1, r.prob*exp(-lambda*draw*complier)), 
-                rbinom(n, 1, r.prob*exp(-lambda*draw*complier)))  
+  response <- c(rbinom(n, 1, runif(1,0,0.01)*amp + r.prob*exp(-lambda*draw*complier)), 
+                rbinom(n, 1, runif(1,0,0.02)*amp + r.prob*exp(-lambda*draw*complier)))  
 
+  # Tranform inputs 
+  draw.scaled <- (draw-mean(draw))/(2*sd(draw)) # center and divide by 2 sds
+  complier.center <- complier-mean(complier) # center binary variables
+  lawton <- ifelse(county=="Lawton",1,0)
+  lawton.center <- lawton-mean(lawton)
+  
   # Fit the model
-  fit <- lm(response ~ draw + complier + factor(county))
+  fit <- lm(response ~ draw.scaled + complier.center + lawton.center)
   
   # Return p value
-  return(summary(fit)$coef[,4]['draw']) 
+  return(summary(fit)$coef[,4][2]) 
 }
 
 # Run simulation
@@ -54,7 +61,10 @@ p.vals.bin <- replicate(L,
                          sapply(1:nrow(grid.bin), function(i){
                            SimRegression(grid.bin$r.prob[i],
                                          grid.bin$s.size[i],
-                                         lambda, n, comply)}))
+                                         lambda, 
+                                         n, 
+                                         comply,
+                                         grid.bin$amp[i])}))
 
 p.vals.bin.array <- t(sapply(1:L, function(i) array(p.vals.bin[,i])))  # rows: iterations
 
@@ -68,10 +78,11 @@ grid.bin$power <- apply(p.vals.bin.array, 2, function (x) length(which(x < alpha
 
 power.plot.bin <- ggplot(data=grid.bin, aes(x=s.size, 
                                             y=power, 
-                                            group = as.factor(r.prob), 
-                                            colour = as.factor(r.prob))) +
+                                            colour = as.factor(r.prob),
+                                            linetype = as.factor(amp))) +
   geom_line() +
   scale_colour_discrete(name = "Effect size", labels=c("1.25%","2.5%","5%","10%")) +
+  scale_linetype_discrete(name = "Noise", labels=c("None","Low","High")) +
   scale_x_continuous(breaks=s.size, labels = c("500", "1,000", "2,000", "4,000", "8,000", "13,000")) +
   scale_y_continuous(breaks=c(0.25,0.50,0.75,0.8,1), labels = c("25%", "50%", "75%","80%","100%")) +
   geom_hline(yintercept = 0.8, colour="black", linetype = "longdash") +
