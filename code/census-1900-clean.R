21656220427b9eee5789c2bb36e0ea443ece1d58
jvpoulos <jvpoulos@gmail.com>
2017-03-20 14:09:22 -0700

    combine 1900 census in cloud

--- code/census-1900-clean.R
+++ code/census-1900-clean.R
@@ -5,7 +5,7 @@
 ## 100% Sample
 # Load 1900 100% sample and clean
 unzip(paste0(data.directory, "census-1900-100-sample.csv.zip"), exdir=data.directory) # unzip sample
-census.00 <- read.csv(paste0(data.directory,"census-1900-100-sample.csv"),header=TRUE, sep = ",") 
+c.1900.s <- read.csv(paste0(data.directory,"census-1900-100-sample.csv"),header=TRUE, sep = ",") 
 
 # Clean
-census.00 <- CleanIpums(census.00,complete = TRUE)
+c.1900.s <- CleanIpums(c.1900.s)
