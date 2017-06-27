cd79ac7df0a7365f4125ac5f0e23e95f8e3764bb
jvpoulos <jvpoulos@gmail.com>
2017-06-27 12:41:38 -0400

    finish did

--- code/did.R
+++ code/did.R
@@ -13,13 +13,60 @@ patents.sum.did <- patents %>%
 # Category
 patents.sum.did$cat <- ifelse(patents.sum.did$state_code=="OK" & patents.sum.did$county_code %in% c(ok.lottery), "Treated", "Control") # compare lottery counties vs. all other
 
+cats.sums.did <- patents.sum.did %>% 
+  group_by(date,cat) %>% 
+  summarise_each(funs(sum),sales,homesteads) 
+
 # Create var for when treatment started
 
-patents.sum.did$time <- 0
-patents.sum.did$time[patents.sum.did$date >= "Jul 1901"] <- 1
+cats.sums.did$time <- 0
+cats.sums.did$time[cats.sums.did$date >= "Jul 1901"] <- 1
 
-did.sales <- lm(sales ~ cat*time, data = patents.sum.did)
+# Sales 
+did.sales <- lm(sales ~ cat*time, data = cats.sums.did[cats.sums.did$date >= "Aug 1891",]) # subset to when treated counties exist
 
 summary(did.sales)
 
-confint(did.sales)
+confint(did.sales)[4,]
+
+# Homesteads 
+did.homesteads <- lm(homesteads ~ cat*time, data = cats.sums.did[cats.sums.did$date >= "Aug 1891",]) # subset to when treated counties exist
+
+summary(did.homesteads)
+
+confint(did.homesteads)[4,]
+
+## Census
+
+c.county <- RbindMatchColumns(df1, df8)[c("year","state", "county", "G", "tenancy")] #1890-1950 # Gini & Tenancy
+
+# Year to time
+
+c.county$date <- as.yearmon(paste0("12/01/",c.county$year), "%m/%d/%Y",tz="UTC") # convert to monthly data
+
+# Create county category var 
+
+c.county$cat <- ifelse(c.county$state==53 & c.county$county %in% c(ok.lottery), "Treated", "Control") # compare lottery counties vs. all other
+
+cats.sums.did <- c.county %>% 
+  group_by(date,cat) %>% 
+  summarise_each(funs(mean(., na.rm = TRUE)),G, tenancy) 
+
+# Create var for when treatment started
+
+cats.sums.did$time <- 0
+cats.sums.did$time[cats.sums.did$date > "Dec 1900"] <- 1
+
+# gini 
+did.gini <- lm(G ~ cat*time, data = cats.sums.did) # subset to when treated counties exist
+
+summary(did.gini)
+
+confint(did.gini)[4,]
+
+# tenancy 
+did.tenancy <- lm(tenancy ~ cat*time, data = cats.sums.did) # subset to when treated counties exist
+
+summary(did.tenancy)
+
+confint(did.tenancy)[4,]
